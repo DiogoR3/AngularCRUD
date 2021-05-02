07eb955d1c3c7f77566b4ae4f0bdd031fd17245d
Diogo Rodrigues <diogo.rodcarreira@hotmail.com>
2021-05-02 16:28:57 -0300

    Adicionado controller Game

---
+++ Backend/Controllers/GameController.cs
@@ -0,0 +1,57 @@
+﻿using Backend.Models;
+using Backend.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        private readonly IGenericService<Game> GameService;
+
+        public GameController(IGenericService<Game> gameService)
+        {
+            GameService = gameService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Game>>> ListAsync()
+        {
+            return await GameService.ListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetAsync(int id)
+        {
+            Game game = await GameService.GetAsync(id);
+            return game is null ? NotFound() : game;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Game>> CreateAsync(Game game)
+        {
+            Game newGame = await GameService.CreateAsync(game);
+            return CreatedAtAction("Create", newGame);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Game>> CreateOrUpdateAsync(int id, Game game)
+        {
+            if (game?.Id != id)
+                return BadRequest();
+
+            await GameService.UpdateAsync(id, game);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Game>> DeleteAsync(int id)
+        {
+            await GameService.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
--- Backend/Service/GenericService.cs
+++ Backend/Service/GenericService.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.Models;
 using Backend.Repository;
-using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Service
 {
